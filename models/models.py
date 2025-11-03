4b38ab387cd4e84090118d23475bc8eaff5cc103
Teo2237 <127605714+Teo2237@users.noreply.github.com>
2025-11-03 17:39:35 +0000

    Created User model (working :)) )

--- models/models.py
+++ models/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlmodel import Field, SQLModel
 
 
@@ -6,3 +8,12 @@ class Hero(SQLModel, table=True):
     name: str
     secret_name: str
     age: int | None = None
+
+
+class User(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    email: str = Field(unique=True)
+    username: str = Field(unique=True)
+    hashed_password: str
+    is_active: bool = Field(default=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
