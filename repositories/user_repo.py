b69b0458770f630653275b3b948998fc8a9c7387
Teo2237 <127605714+Teo2237@users.noreply.github.com>
2025-11-03 18:49:11 +0000

    Added CRUD repository for Users

--- repositories/user_repo.py
+++ repositories/user_repo.py
@@ -1,6 +1,28 @@
+from fastapi import HTTPException
 from sqlmodel import Session, select
 
-from models.models import Hero
+from models.models import Hero, User
+
+
+def get_user_by_email(session: Session, email: str) -> User | None:
+    user = session.exec(select(User).where(User.email == email)).one()
+    if not user:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return user
+
+
+def get_user_by_username(session: Session, username: str) -> User | None:
+    user = session.exec(select(User).where(User.username == username)).one()
+    if not user:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return user
+
+
+def create_user(session: Session, user: User) -> User | None:
+    session.add(User)
+    session.commit()
+    session.refresh(user)
+    return user
 
 
 def get_hero_by_id(session: Session, hero_id: int) -> Hero | None:
