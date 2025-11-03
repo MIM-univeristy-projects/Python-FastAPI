ea5840d05197aa5514af2f75ed96fa8bc039cecc
NaviteLogger <kacprzakmarek92@gmail.com>
2025-11-03 11:17:58 +0000

    Enabled ruff for the workspace

--- database/database.py
+++ database/database.py
@@ -1,10 +1,17 @@
+from collections.abc import Generator
+
 from sqlalchemy import Engine
 from sqlmodel import Session, create_engine
 
-postgres_url = "postgresql://postgres:postgres@postgres-devcontainer:5432/postgres"
+postgres_url: str = "postgresql://postgres:postgres@postgres-devcontainer:5432/postgres"
 engine: Engine = create_engine(postgres_url, echo=True)
 
 
-def get_session():
+def get_session() -> Generator[Session, None, None]:
+    """Returns (yields) the opened session to interact with the database.
+
+    Yields:
+        Session: an open database session
+    """
     with Session(engine) as session:
         yield session
